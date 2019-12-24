d523ace224e5f5329d4b207bba451a8f3a2f1d60
ctfairhousing <59203795+ctfairhousing@users.noreply.github.com>
2019-12-24 09:27:10 -0500

    Add files via upload

---
+++ chocolatey.ps1
@@ -0,0 +1,23 @@
+﻿$ChocoPackages = @(“googlechrome”,"ublockorigin-chrome”,"7zip”,”notepadplusplus.install”,”7zip.install”,"firefox","ublockorigin-firefox", "paint.net")
+
+$ChocoInstall = Join-Path ([System.Environment]::GetFolderPath(“CommonApplicationData”)) “Chocolatey\bin\choco.exe”
+
+
+if(!(Test-Path $ChocoInstall)) {
+        try {
+
+            Invoke-Expression ((New-Object net.webclient).DownloadString(‘https://chocolatey.org/install.ps1’)) -ErrorAction Stop
+        }
+        catch {
+            Throw “Failed to install Chocolatey”
+        }      
+}
+
+foreach($Package in $ChocoPackages) {
+    try {
+        Invoke-Expression “cmd.exe /c $ChocoInstall Install $Package -y” -ErrorAction Stop
+    }
+    catch {
+        Throw “Failed to install $Package”
+    }
+}
